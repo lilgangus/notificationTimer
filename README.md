b928982dfe316c1f1210f6f90becc73fe18dba2e
lilgangus <tangcharles29@gmail.com>
2023-03-18 16:47:22 -0700

    done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cliNotificationTimer
- 
+This script is a simple timer that will notify you when 20 minutes, 1200 seconds, has passed. If you click "OK" on the notification, the timer will reset. If you click "Cancel", the timer will stop. If you do nothing, after 60 seconds the timer will restart.
---
+++ timer.sh
@@ -0,0 +1,15 @@
+# #!/bin/bash
+
+while true
+do
+  osascript -e 'display notification "20 miutes have passed, take a break" with title "Timer"'
+  osascript -e 'tell application "System Events" to display dialog "Do you want to start the timer again?" with title "Timer" buttons {"OK", "Cancel"} default button 1 giving up after 60' > /dev/null
+  button=$(echo $?)
+  if(( $button == 0 ))
+  then
+    sleep 1200 # wait for 20 minutes
+  else
+    exit 0 # end the script if Cancel button is clicked
+  fi
+done
+
